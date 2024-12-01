a4676743c6ed6f3036d299e42a54c87b5bc37ed7
Yurii <Jahnavart@gmail.com>
2024-12-01 12:13:43 +0200

    with coment JS

---
+++ js/task-modul.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// const courseTopic = "JavaScript essentials";
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic [0];
+// const lastElement = (courseTopic[courseTopic.length - 1]);
+
+// console.log(courseTopicLength); //21
+// console.log(firstElement); //J
+// console.log(lastElement); //s
+
+
+function makeMessage(username) {
+	console.log(`Hello ${username}`);
+}
+
+makeMessage("Jacob")
+
+
+
